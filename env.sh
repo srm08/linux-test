73bc58952a69fc6e4d21e6f15ed51927a00d129f
Sugasini Ramaswamy <87127475+srm08@users.noreply.github.com>
2024-11-12 20:53:24 +1100

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##########################################################################
+#                                                                        #
+#                                                                        #
+#      Environment  Variables File to allow single code to be used       #
+#      RHEL on EC2 											                           			 #
+#                                                                        #
+#       
+#                                                                        #
+#                                                                        #
+##########################################################################
+                                                                       
+#!/bin/bash
+
+#Setting the Environment Variables based on Bamboo plan
+NGINX_WEBPORT=80
+NGINX_VERSION=1.20.1-1
